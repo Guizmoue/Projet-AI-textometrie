6fa9f3363f1006905fa2144c2293367cf40e55b8
Zhongjie LI <lzj44010@gmail.com>
2023-12-09 21:19:37 +0100

    token_chinois_projetPPEbis

--- programmes/projetPPE_bis.sh
+++ programmes/projetPPE_bis.sh
@@ -6,7 +6,7 @@ then
     exit
 fi
 
-# cd ./URLs/ # pourquoi on a besoin d'aller dans le fichier URLs ?
+cd ../URLs/ # pourquoi on a besoin d'aller dans le fichier URLs ?
 
 URLS="$1"
 
@@ -17,9 +17,9 @@ fi
 
 lineno=1
 lang=$(basename $URLS .txt)
-html_file="./tableaux/$lang.html"
+html_file="../tableaux/$lang.html"
 #motif="intelligence\sartificielle"
-motif="(\bIA\b|intelligence\sartificielle|\bAI\b|artificial\sintelligence|人工智能|人工智慧)"
+motif="(\bIA\b|intelligence\sartificielle|\bAI\b|artificial\sintelligence|人工\s?智能|人工\s?智慧)"
 
 
 echo "<html><head></head><body>" > $html_file
@@ -30,48 +30,43 @@ while read -r URL
 do
     response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
     encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | ggrep -P -o "charset=\S+" | cut -d"=" -f2)
-    aspi=$(curl -s -L -o "./aspirations/${lang}-${lineno}.html" $URL)
+    aspi=$(curl -s -L -o "../aspirations/${lang}-${lineno}.html" $URL)
 
     compte=0
     TEXTFILE="NA"
 
     if [ $response -eq 200 ]
     then
-        if [ ! $encoding == "UTF-8"]
+
+        
+
+        if [ ! "${lang}" = "cn" ]
         then
-            iconv -f "${encoding}" -t "UTF-8" -o "/tmp/recode_${lineno}.html" "./aspirations/${lang}-${lineno}.html"
-            mv "/tmp/recode_${lineno}.html" "./aspirations/${lang}-${lineno}.html"
+            # création du dump text
+            lynx -assume_charset=utf-8 -display_charset=utf-8 -dump -nolist $URL > "../dumps-text/${lang}-${lineno}.txt"
+            TEXTFILE="../dumps-text/${lang}-${lineno}.txt"
         fi
 
-        # création du dump text
-        lynx -assume_charset UTF-8 -dump -nolist "./aspirations/${lang}-${lineno}.html" > "./dumps-text/${lang}-${lineno}.txt"
-
-        TEXTFILE="./dumps-text/${lang}-${lineno}.txt"
-        compte=$(ggrep -P -i -o $motif "./dumps-text/${lang}-${lineno}.txt" | wc -l)
+        if [ "${lang}" == "cn" ]
+        then
+            w3m $URL > ../dumps-text/${lang}-${lineno}.txt
+            TEXTFILE=../dumps-text/${lang}-${lineno}.txt
+            python -m thulac ${TEXTFILE} ../dumps-text/${lang}-${lineno}_token.txt -seg_only
+            TEXTFILE=../dumps-text/${lang}-${lineno}_token.txt
+            
+        fi
+        compte=$(ggrep -P -i -o $motif "../dumps-text/${lang}-${lineno}.txt" | wc -l)
 
         # recherche des contextes
-        cat $TEXTFILE | ggrep -P -i -A 1 -B 1 "${motif}" > "./Contextes/${lang}-${lineno}.txt"
+        cat $TEXTFILE | ggrep -P -i -A 1 -B 1 "${motif}" > "../Contextes/${lang}-${lineno}.txt"
 
-        if [ ! ${lang} = "cn" ]
-        then
-            # création du concordancier
-            echo "<html><head></head><body>" > "./concordances/${lang}-${lineno}.html"
-            echo "<table border='1'>" >> "./concordances/${lang}-${lineno}.html"
-            echo "<tr><th>Contexte gauche</th><th>Mot</th><th>Contexte droit</th><th></tr>" >> "./concordances/${lang}-${lineno}.html"
-            grep -E -i -o "(\w+\W+){0,5}$motif(\W+\w+){0,5}" $TEXTFILE | sed -E 's/(.*)(\bIA\b|intelligence\sartificielle|\bAI\b|artificial\sintelligence)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/' >> "./concordances/${lang}-${lineno}.html"
-            echo "</table></body></html>" >> "./concordances/${lang}-${lineno}.html"
-        fi
-
-        if [ ${lang} = "cn" ]
-        then
-            motif="(人工 智能|人工 智慧)"
-            echo "<html><head></head><body>" > "./concordances/${lang}-${lineno}.html"
-            echo "<table border='1'>" >> "./concordances/${lang}-${lineno}.html"
-            echo "<tr><th>Contexte gauche</th><th>Mot</th><th>Contexte droit</th><th></tr>" >> "./concordances/${lang}-${lineno}.html"
-            cat $TEXTFILE | python3 ./programmes/chinois/tokenize_chinese.py | grep -E -i -o "(\w+\W+){0,5}$motif(\W+\w+){0,5}" $TEXTFILE | sed -E 's/(.*)(人工 智能|人工 智慧)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/' >> "./concordances/${lang}-${lineno}.html"
-            echo "</table></body></html>" >> "./concordances/${lang}-${lineno}.html"
-        fi
-        motif="(\bIA\b|intelligence\sartificielle|\bAI\b|artificial\sintelligence|人工智能|人工智慧)"
+        # création du concordancier
+        echo "<html><head></head><body>" > "../concordances/${lang}-${lineno}.html"
+        echo "<table border='1'>" >> "../concordances/${lang}-${lineno}.html"
+        echo "<tr><th>Contexte gauche</th><th>Mot</th><th>Contexte droit</th><th></tr>" >> "../concordances/${lang}-${lineno}.html"
+        grep -E -i -o "(\w+\W+){0,5}$motif(\W+\w+){0,5}" $TEXTFILE | sed -E 's/(.*)(\bIA\b|intelligence\sartificielle|\bAI\b|artificial\sintelligence|人工智能|人工智慧)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/' >> "../concordances/${lang}-${lineno}.html"
+        echo "</table></body></html>" >> "../concordances/${lang}-${lineno}.html"
+        
     fi
 
     echo "<tr>
@@ -79,11 +74,11 @@ do
     <td>$URL</td>
     <td>$response</td>
     <td>$encoding</td>
-    <td><a href ="./aspirations/${lang}-${lineno}.html">Aspiration</a></td>
-    <td><a href ="./dumps-text/${lang}-${lineno}.txt">Dump</a></td>
+    <td><a href ="../aspirations/${lang}-${lineno}.html">Aspiration</a></td>
+    <td><a href ="../dumps-text/${lang}-${lineno}.txt">Dump</a></td>
     <td>$compte</td>
-    <td><a href="./Contextes/${lang}-${lineno}.txt">Contextes</a></td>
-    <td><a href="./concordances/${lang}-${lineno}.html">Concordances</a></td>
+    <td><a href="../Contextes/${lang}-${lineno}.txt">Contextes</a></td>
+    <td><a href="../concordances/${lang}-${lineno}.html">Concordances</a></td>
     </tr>" >> $html_file
     lineno=$(expr $lineno + 1)
 done < $URLS
