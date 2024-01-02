a3375bbe5eb5ef8d6ab244d6224562ce15aa9944
Guizmoue <guilhem.schlosser@gmail.com>
2024-01-02 19:14:11 +0100

    modif question.html/js

--- question.js
+++ question.js
@@ -1,4 +1,4 @@
-function sendmail(){
+function sendMail(){
     var parameters = {
         firstname: document.getElementById("firstname").value ,
         lastname: document.getElementById("lastname").value ,
@@ -7,19 +7,20 @@ function sendmail(){
         objet: document.getElementById("objet").value ,
         message: document.getElementById("message").value ,
     };
+
     const service_id = "service_bj56b5v";
     const template_id = "template_xejwlns";
     
     emailjs.send(service_id, template_id, parameters).then(
         responce => {
-            document.getElementsById("firstname").value = "";
-            document.getElementsById("lastname").value = "";
-            document.getElementsById("email").value = "";
-            document.getElementsById("telephone").value = "";
-            document.getElementsById("objet").value = "";
-            document.getElementsById("message").value = "";
+            document.getElementById("firstname").value = "";
+            document.getElementById("lastname").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("telephone").value = "";
+            document.getElementById("objet").value = "";
+            document.getElementById("message").value = "";
             console.log(responce);
             alert("Votre message a été envoyé avec succès !");
         })
-        .catch((err) => console.log(err));
+        .catch((erreur) => console.log(erreur));
 }
