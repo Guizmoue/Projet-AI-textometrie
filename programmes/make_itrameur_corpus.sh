fb08905888d6a2d967dd16ccb66d433ac19de315
AmandineJouvenel <amandine.jouvenel@sorbonne-nouvelle.fr>
2024-01-06 21:14:32 +0100

    annotation du script make_itrameur_corpus.sh

--- programmes/make_itrameur_corpus.sh
+++ programmes/make_itrameur_corpus.sh
@@ -1,3 +1,7 @@
+# Ce programme permet d'obtenir des fichiers en pseudo-XML pouvant être traités par iTrameur.
+# Il prend en entrée un dossier contenant les fichiers de contextes de ontre mots, et la langue.
+# Les fichiers sont compilés en un seul fichier utilisé par la suite dans iTrameur.
+
 #!/usr/bin/env bash
 
 # vérification des arguments
@@ -7,15 +11,18 @@ then
 	exit
 fi
 
+# stockage des arguments dans des variables
 dossier=$1
 basename=$2
 
 echo "<lang=\"$basename\">" > "../itrameur/$dossier-$basename.txt"
 
+# Pour chaque fichier du dossier, on effectue les actions suivantes :
 for chemin in $(ls ../$dossier/$basename-*.txt)
 do
 	pagename=$(basename -s .txt $chemin)
 
+	# format pseudo-XML pour le fichier de sortie
 	echo "<page=\"$pagename\">" >> "../itrameur/$dossier-$basename.txt"
 	echo "<text>" >> "../itrameur/$dossier-$basename.txt"
 
@@ -27,10 +34,11 @@ do
 	content=$(echo "$content" | sed 's/</&lt;/g')
 	content=$(echo "$content" | sed 's/>/&gt;/g')
 
+	# format pseudo-XML pour le fichier de sortie
 	echo "$content" >> "../itrameur/$dossier-$basename.txt"
-
 	echo "</text>" >> "../itrameur/$dossier-$basename.txt"
 	echo "</page> §" >> "../itrameur/$dossier-$basename.txt"
 done
 
+# format pseudo-XML pour le fichier de sortie
 echo "</lang>" >> "../itrameur/$dossier-$basename.txt"
